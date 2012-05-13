1a1cba50a09ad78542aa718099814b8905bb84f1
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2012-05-13 18:32:35 +0300

    Extract 'normalize delimiters' method

--- calculator.py
+++ calculator.py
@@ -1,13 +1,17 @@
 def add(string):
+  string = _normalize_delimiters(string)
+  if string:
+    return _add_numbers_in_string(string)
+  else:
+    return 0
+
+def _normalize_delimiters(string):
   if string.startswith('//'):
     delimiter = string[2]
     string = string[4:]
     string = string.replace(delimiter, ',')
   string = string.replace('\n', ',')
-  if string:
-    return _add_numbers_in_string(string)
-  else:
-    return 0
+  return string
 
 def _add_numbers_in_string(string):
   return sum(map(int, string.split(',')))
