2d46eef33ba7ad81596effaa74ee754e7c564bfc
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2012-05-13 18:41:00 +0300

    Extract 'validate numbers' method

--- calculator.py
+++ calculator.py
@@ -19,6 +19,9 @@ def _normalize_custom_delimiter(string):
 
 def _add_numbers_in_string(string):
   numbers = map(int, string.split(','))
+  _validate_numbers(numbers)
+  return sum(numbers)
+
+def _validate_numbers(numbers):
   if any(number < 0 for number in numbers):
     raise ValueError
-  return sum(numbers)
